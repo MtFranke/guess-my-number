5f98fb92eadcf4edbb38197404035c2556970a57
mateuszfranke <mateuszfranke@icloud.com>
2021-02-26 19:18:03 +0100

    guess number feature

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const luckNumber = Math.trunc((Math.random() * 20)+1);
+console.log(luckNumber);
+
+
+const guessEvent = function () {
+    var guess = Number(document.querySelector('.guess').value);
+    if(guess){
+        if(guess === luckNumber) console.log('You guessed the lucky number 👌');
+    }else console.log('Try again 💩')
+}
+
+document.querySelector('.check').addEventListener('click', guessEvent);
