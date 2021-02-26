1bc2b33a96f67e791c4506b7fe4a211960e4e8af
mateuszfranke <mateuszfranke@icloud.com>
2021-02-26 19:38:55 +0100

    more game logic

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 'use strict';
-
-const luckNumber = Math.trunc((Math.random() * 20)+1);
-console.log(luckNumber);
+let score = 20;
+const luckNumber = Math.trunc((Math.random() * 20) + 1);
+document.querySelector('.number').textContent = luckNumber;
 
 
 const guessEvent = function () {
     var guess = Number(document.querySelector('.guess').value);
-    if(guess){
-        if(guess === luckNumber) console.log('You guessed the lucky number 👌');
-    }else console.log('Try again 💩')
+    if (guess) {
+        if (guess === luckNumber) document.querySelector('.message').textContent = 'You guessed the lucky number 👌';
+        else { 
+            guess > luckNumber ? document.querySelector('.message').textContent = 'To High! 📈': document.querySelector('.message').textContent = 'To Low! 📉';
+            decreaseScore();
+        }
+    }
+}
+
+const decreaseScore = function () {
+    
+    if(score > 0) score -= 1;
+    else document.querySelector('.message').textContent = 'You lost the game 😭';
+    document.querySelector('.score').textContent = score;
 }
 
 document.querySelector('.check').addEventListener('click', guessEvent);
