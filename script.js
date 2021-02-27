e0794fe99fbc53942515b4c878237f67f0738000
mateuszfranke <mateuszfranke@icloud.com>
2021-02-27 11:07:49 +0100

    css manipulation

--- script.js
+++ script.js
@@ -2,20 +2,23 @@
 let score = 20;
 const scores =[];
 const getLuckyNumber = () => Math.trunc((Math.random() * 20) + 1);
-const luckNumber = getLuckyNumber();
-document.querySelector('.number').textContent = luckNumber;
+let luckNumber = getLuckyNumber();
 
+const win = function(){
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = luckNumber;
+    document.querySelector('.message').textContent = 'You guessed the lucky number 👌';
+    scores.push(score);
+    scores.sort();
+    document.querySelector('.highscore').textContent = scores[(scores.length)-1];
+}
 
 const guessEvent = function () {
     var guess = Number(document.querySelector('.guess').value);
     if (guess) {
         if (guess === luckNumber){
-            document.querySelector('.message').textContent = 'You guessed the lucky number 👌';
-            scores.push(score);
-            scores.sort();
-            document.querySelector('.highscore').textContent = scores[(scores.length)-1];
-            luckNumber = getLuckyNumber();
-            document.querySelector('.number').textContent = luckNumber;
+            win();
         } 
         else { 
             guess > luckNumber ? document.querySelector('.message').textContent = 'To High! 📈': document.querySelector('.message').textContent = 'To Low! 📉';
@@ -25,10 +28,15 @@ const guessEvent = function () {
 }
 
 const againEvent = function(){
-    score = 20;
+    score = 20; 
     document.querySelector('.score').textContent =  score;
     document.querySelector('.message').textContent = 'Start guessing...';
     document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    luckNumber = getLuckyNumber();
+    document.querySelector('.number').textContent = '?';
+
 }
 
 const decreaseScore = function () {
@@ -38,7 +46,6 @@ const decreaseScore = function () {
 
     document.querySelector('.score').textContent = score;
 }
-
 
 document.querySelector('.check').addEventListener('click', guessEvent);
 document.querySelector('.again').addEventListener('click', againEvent);
